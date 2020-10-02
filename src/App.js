76e567f7c09f690ae10bd5336c58d98294e7a602
Siddhika Rakshe <siddhikarakshe@gmail.com>
2020-10-02 14:55:18 +0530

    added twitter icon in sidebar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
-
-import './App.css';
+import Sidebar from './Sidebar';
+import "./App.css" ;
 
 function App() {
   return (
     <div className="app">
       <h1>lets build tweety</h1>
       {/* Sidebar */}
+     <Sidebar/>
 
       {/* feed */}
 
