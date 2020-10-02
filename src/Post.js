3dcb8c42657dacee1140b596abc7c1ba0dd49641
Siddhika Rakshe <siddhikarakshe@gmail.com>
2020-10-02 23:14:47 +0530

    removeparsing erros

--- src/Post.js
+++ src/Post.js
@@ -2,6 +2,10 @@ import React from "react";
 import "./Post.css";
 import { Avatar } from "@material-ui/core";
 import VerifiedUserIcon from "@material-ui/icons/VerifiedUser";
+import ChatBubbleOutlineIcon from "@material-ui/icons/ChatBubbleOutline";
+import RepeatIcon from "@material-ui/icons/Repeat";
+import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
+import PublishIcon from "@material-ui/icons/Publish";
 
 
 
@@ -29,10 +33,18 @@ function Post({
                     </span>
 
                     </h3>
-
-                </div>
-
-            </div>
+                    </div>
+                    <div  className="post__headerDescription">
+                   <p>{text }</p>
+                   </div>
+                   </div>
+                   <img src="" alt=""/>
+                   <div className="post__footer">
+                   <ChatBubbleOutlineIcon fontSize="small" />
+                   <RepeatIcon fontSize="small" />
+                   <FavoriteBorderIcon fontSize="small" />
+                   <PublishIcon fontSize="small" />
+                   </div>
 
         </div>
 
