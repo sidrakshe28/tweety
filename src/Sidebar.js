3709d35330442bd5b50fa10a3a56a0abf31dffcf
Siddhika Rakshe <siddhikarakshe@gmail.com>
2020-10-02 15:45:37 +0530

    added all sidebar icons

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -5,6 +5,12 @@ import SidebarOption from "./SidebarOption";
 import HomeIcon from '@material-ui/icons/Home';
 import SearchIcon from '@material-ui/icons/Search';
 import NotificationsNoneIcon from '@material-ui/icons/NotificationsNone';
+import MailOutlineIcon from "@material-ui/icons/MailOutline";
+import BookmarkBorderIcon from "@material-ui/icons/BookmarkBorder";
+import ListAltIcon from "@material-ui/icons/ListAlt";
+import PermIdentityIcon from "@material-ui/icons/PermIdentity";
+import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
+import { Button } from "@material-ui/core";
 
  
 
@@ -18,6 +24,11 @@ function Sidebar() {
             <SidebarOption Icon={ HomeIcon} text="Home"/>
             <SidebarOption Icon={ SearchIcon} text="Explore" />
             <SidebarOption Icon={NotificationsNoneIcon } text="Notifications"/>
+            <SidebarOption Icon={MailOutlineIcon} text="Messages" />
+      <SidebarOption Icon={BookmarkBorderIcon} text="Bookmarks" />
+      <SidebarOption Icon={ListAltIcon} text="Lists" />
+      <SidebarOption Icon={PermIdentityIcon} text="Profile" />
+      <SidebarOption Icon={MoreHorizIcon} text="More" />
             
             { /*side bar options*/}
             
