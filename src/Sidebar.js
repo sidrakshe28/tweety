fe18c2b820c8df52fe7f2a15ec6b645c1685a453
Siddhika Rakshe <siddhikarakshe@gmail.com>
2020-10-02 14:40:16 +0530

    imported twitter icon

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,11 +1,14 @@
 import React from "react"
 import './Sidebar.css';
+import TwitterIcon from '@material-ui/icons/Twitter';
 
 
 function Sidebar() {
     return (
         <div className="sidebar">
-            {/* Twitter icon */}
+            
+            <TwitterIcon/>
+            
             { /*side bar options*/}
             
         </div>
