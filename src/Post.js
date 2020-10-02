b872deda47d6e5203484162d11f217ca0bb490b3
Siddhika Rakshe <siddhikarakshe@gmail.com>
2020-10-03 01:30:25 +0530

    removed erros related to firebase

--- src/Post.js
+++ src/Post.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { forwardRef } from "react";
 import "./Post.css";
 import { Avatar } from "@material-ui/core";
 import VerifiedUserIcon from "@material-ui/icons/VerifiedUser";
@@ -7,50 +7,39 @@ import RepeatIcon from "@material-ui/icons/Repeat";
 import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
 import PublishIcon from "@material-ui/icons/Publish";
 
-
-
-function Post({
-    displayname,
-    username,
-    verified,
-    timestamp,
-    text,
-    image,
-    avatar
-}) {
+const Post = forwardRef(
+  ({ displayName, username, verified, text, image, avatar }, ref) => {
     return (
-        <div className="post">
+      <div className="post" ref={ref}>
         <div className="post__avatar">
-            
-            <Avatar src={avatar} />
-            
+          <Avatar src={avatar} />
         </div>
         <div className="post__body">
-            <div className="post__header">
-                <div className="post__headerText">
-                    <h3> {displayname} { " "}
-                     <span className="post__headerSpecial">
-                     {verified && <VerifiedUserIcon className="post__badge" /> @{username}
-                    </span>
-
-                    </h3>
-                    </div>
-                    <div  className="post__headerDescription">
-                   <p>{text}</p>
-                   </div>
-                   </div>
-                   <img src={image} alt=""/>
-                   <div className="post__footer">
-                   <ChatBubbleOutlineIcon fontSize="small" />
-                   <RepeatIcon fontSize="small" />
-                   <FavoriteBorderIcon fontSize="small" />
-                   <PublishIcon fontSize="small" />
-                   </div>
-
+          <div className="post__header">
+            <div className="post__headerText">
+              <h3>
+                {displayName}{" "}
+                <span className="post__headerSpecial">
+                  {verified && <VerifiedUserIcon className="post__badge" />} @
+                  {username}
+                </span>
+              </h3>
+            </div>
+            <div className="post__headerDescription">
+              <p>{text}</p>
+            </div>
+          </div>
+          <img src={image} alt="" />
+          <div className="post__footer">
+            <ChatBubbleOutlineIcon fontSize="small" />
+            <RepeatIcon fontSize="small" />
+            <FavoriteBorderIcon fontSize="small" />
+            <PublishIcon fontSize="small" />
+          </div>
         </div>
-
-        </div>
+      </div>
     );
-}
+  }
+);
 
 export default Post;
--- src/TweetBox.js
+++ src/TweetBox.js
@@ -1,25 +1,59 @@
- import React from "react";
- import "./TweetBox.css";
- import { Avatar, Button } from "@material-ui/core";
+import React, { useState } from "react";
+import "./TweetBox.css";
+import { Avatar, Button } from "@material-ui/core";
+import db from "./Firebase";
 
- 
- function TweetBox() {
-    return (
-        <div className="tweetBox">
-          <form>
-            <div className="tweetBox__input">
-              <Avatar src="https://ibb.co/PYKMGPf" />
-              <input placeholder="whats happening?" type="text" />
-              </div>
+function TweetBox() {
+  const [tweetMessage, setTweetMessage] = useState("");
+  const [tweetImage, setTweetImage] = useState("");
 
-              <input className="tweetBox__imageInput" placeholder=" Optional: Enter image URL" type="text" />
-              <Button className="tweetBox__tweetButton "> Tweet</Button>
-            </form>
+  const sendTweet = (e) => {
+    e.preventDefault();
+
+    db.collection("posts").add({
+      displayName: "Siddhika Rakshe",
+      username: "SidRakshe28",
+      verified: true,
+      text: tweetMessage,
+      image: tweetImage,
+      avatar:
+        "https://www.dreamstime.com/software-language-programmer-avatar-software-language-programmer-avatar-vector-illustration-design-image110589785",
+    });
+
+    setTweetMessage("");
+    setTweetImage("");
+  };
+
+  return (
+    <div className="tweetBox">
+      <form>
+        <div className="tweetBox__input">
+          <Avatar src="https://www.dreamstime.com/software-language-programmer-avatar-software-language-programmer-avatar-vector-illustration-design-image110589785" />
+          <input
+            onChange={(e) => setTweetMessage(e.target.value)}
+            value={tweetMessage}
+            placeholder="What's happening?"
+            type="text"
+          />
+        </div>
+        <input
+          value={tweetImage}
+          onChange={(e) => setTweetImage(e.target.value)}
+          className="tweetBox__imageInput"
+          placeholder="Optional: Enter image URL"
+          type="text"
+        />
+
+        <Button
+          onClick={sendTweet}
+          type="submit"
+          className="tweetBox__tweetButton"
+        >
+          Tweet
+        </Button>
+      </form>
     </div>
   );
 }
 
-
- 
- export default TweetBox;
- 
+export default TweetBox;
